ffc271198f1aa568f32cc9245330151a698fe647
cajunSub <junsyub@gmail.com>
2024-04-12 12:07:18 +0900

    최초 푸쉬

---
+++ spring_boot_ajs_project_board/src/main/java/edu/sejong/ex/SpringBootAjsProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package edu.sejong.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAjsProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAjsProjectBoardApplication.class, args);
+	}
+
+}
