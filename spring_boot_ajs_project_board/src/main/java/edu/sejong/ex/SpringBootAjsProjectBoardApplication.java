3d3c5dca66801c8409685cdfb255c87d975bca70
cajunSub <junsyub@gmail.com>
2024-04-12 12:50:51 +0900

    점심 식사 전 VO 및 Mapper(미완성) 추가

--- spring_boot_ajs_project_board/src/main/java/edu/sejong/ex/SpringBootAjsProjectBoardApplication.java
+++ spring_boot_ajs_project_board/src/main/java/edu/sejong/ex/SpringBootAjsProjectBoardApplication.java
@@ -1,11 +1,15 @@
 package edu.sejong.ex;
 
+import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value= {"edu.sejong.ex.mapper"})
 @SpringBootApplication
 public class SpringBootAjsProjectBoardApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAjsProjectBoardApplication.class, args);
 	}
---
+++ spring_boot_ajs_project_board/src/main/java/edu/sejong/ex/mapper/BoardMapper.java
@@ -0,0 +1,9 @@
+package edu.sejong.ex.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+
+@Mapper
+public interface BoardMapper {
+	
+}
